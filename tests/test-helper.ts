85998da50ee874578da8041a0af2752e131d2327
frank <wangsen9108@gmail.com>
2020-03-24 18:21:14 +0800

    clear gh-pages content

--- tests/test-helper.ts
+++
@@ -1,8 +0,0 @@
-import Application from 'search-github/app';
-import config from 'search-github/config/environment';
-import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
-
-setApplication(Application.create(config.APP));
-
-start();
